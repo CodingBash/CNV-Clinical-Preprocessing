1e99e093316e8c15c44695fdc11e678a32039729
CodingBash <bbecerr@outlook.com>
2018-07-08 19:37:54 -0400

    Using ggplot2

--- visualizeGMM.R
+++ visualizeGMM.R
@@ -1,3 +1,5 @@
+install.packages("ggplot2")
+library(ggplot2)
 sample <- "hT1"
 
 segtable <- read.table(paste("segClusteringResults/", sample, "_segtable.tsv", sep = ""), sep = "\t", header = TRUE)
@@ -11,7 +13,7 @@ for(mean in means){
   segmedians <- segtable[segtable$maxzmean == mean, ]$segmedian
   print(paste(mean, "cluster between", min(segmedians), "and", max(segmedians)))
   for(tb.index in seq(1, length(tb))){
-    if(tb[tb.index] >= min(segmedians) - (interval/2) & tb[tb.index] <= max(segmedians) + (interval/2)){
+    if(tb[tb.index] >= min(segmedians) - (interval) & tb[tb.index] <= max(segmedians) + (interval)){
       col[tb.index] <- color  
     } 
   }
@@ -22,13 +24,23 @@ col[which(col==1)] <- "black"
 col[which(col==2)] <- "firebrick1"
 
 col[which(col==3)] <- "gold"
-col[which(col==4)] <- "black"
+col[which(col==4)] <- "purple"
 col[which(col==5)] <- "darkolivegreen1"
 hist(segtable$segmedian,
      main = "Gaussian mixture model of segtable$segmedian,",
      col = col,
      breaks = tb,
-     axes = FALSE)
+     axes = FALSE,
+     prob = TRUE)
+ggplot(data = segtable, aes(segtable$segmedian)) + 
+  geom_histogram(aes(y = ..density..),
+                 breaks = tb,
+                 fill = head(col, length(tb)-1),
+                 alpha = 0.6) + 
+  labs(title = "Gaussian mixture model of segtable$segmedian") +
+  geom_density(col="black")
+
+
 axis(side = 1, at=tb)
 axis(side = 2, at=seq(0, 20, 1))
-#lines(density(segtable$segmedian))
+lines(density(segtable$segmedian))
