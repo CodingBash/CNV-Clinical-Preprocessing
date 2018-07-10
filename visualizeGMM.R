34fce5a8b8abd99dc850f9b5e2a2a38ac28a8ef2
CodingBash <bbecerr@outlook.com>
2018-07-10 09:52:19 -0400

    GMM component distributions colored by cluster assignment

--- visualizeGMM.R
+++ visualizeGMM.R
@@ -27,44 +27,47 @@ for(i in seq(1, length(loaded_samples))){
   sample <- loaded_samples[[i]]
   cd_local()
   
-  segtable <- read.table(paste("segClusteringResults/", sample, "_segtable.tsv", sep = ""), sep = "\t", header = TRUE)
+  segtable <- read.table(paste("segClusteringResults/prev_run1/", sample, "_segtable.tsv", sep = ""), sep = "\t", header = TRUE)
   gaussian_comps <- unique(segtable[c("maxzmean", "maxzsigma")])
   
   interval <- 0.1
   tb <- seq(-3, 3, interval)
   col <- rep(1, length(tb))
-  means <- sort(unique(segtable$maxzmean))
+  means <- unique(segtable$maxzmean)
   color <- 2
   for(mean in means){
-    segmedians <- segtable[segtable$maxzmean == mean, ]$segmedian
-    print(paste(mean, "cluster between", min(segmedians), "and", max(segmedians)))
+    mediandevs <- segtable[segtable$maxzmean == mean, ]$mediandev
+    print(paste(mean, "cluster between", min(mediandevs), "and", max(mediandevs)))
     for(tb.index in seq(1, length(tb))){
-      if(tb[tb.index] >= min(segmedians) - (interval) & tb[tb.index] <= max(segmedians) + (interval)){
+      if(tb[tb.index] >= min(mediandevs) - (interval) & tb[tb.index] <= max(mediandevs) + (interval)){
         col[tb.index] <- color  
       } 
     }
     color <- color + 1
   }
   
-  col[which(col==1)] <- "black"
-  col[which(col==2)] <- "firebrick1"
+  colors <- c("black", "firebrick1", "gold", "purple", "darkolivegreen1", "blue", "orange")
+  col[which(col==1)] <- colors[[1]]
+  col[which(col==2)] <- colors[[2]]
   
-  col[which(col==3)] <- "gold"
-  col[which(col==4)] <- "purple"
-  col[which(col==5)] <- "darkolivegreen1"
-  #hist(segtable$segmedian,
-  #     main = "Gaussian mixture model of segtable$segmedian,",
+  col[which(col==3)] <- colors[[3]]
+  col[which(col==4)] <- colors[[4]]
+  col[which(col==5)] <- colors[[5]]
+  col[which(col==6)] <- colors[[6]]
+  col[which(col==7)] <- colors[[7]]
+  #hist(segtable$mediandev,
+  #     main = "Gaussian mixture model of segtable$mediandev,",
   #     col = col,
   #     breaks = tb,
   #     axes = FALSE,
   #     prob = TRUE)
-  plt <- ggplot2::ggplot(data = segtable, aes(segtable$segmedian)) + 
+  plt <- ggplot2::ggplot(data = segtable, aes(segtable$mediandev)) + 
     geom_histogram(aes(y = ..density..),
                    breaks = tb,
                    fill = head(col, length(col)-1),
                    alpha = 0.6) + 
     xlim(c(-3,3)) +
-    labs(title = "Gaussian mixture model of segtable$segmedian") +
+    labs(title = "Gaussian mixture model of segtable$mediandev") +
     geom_density(col="black", stat = "density")
   
   
@@ -77,10 +80,10 @@ for(i in seq(1, length(loaded_samples))){
     a <- rnorm(1000, mean, sd)
     components[[components.index]] <- a
     components.index <- components.index + 1
-    plt <- plt + stat_function(fun = dnorm, args = list(mean = mean, sd = sd))
+    plt <- plt + stat_function(fun = dnorm, args = list(mean = mean, sd = sd), col = colors[gaussian_comps.index + 1])
   }
   
-  pdf(paste("GMMs/plots_", sample, ".pdf", sep = ""))
+  pdf(paste("GMMs/mediandev/plots_", sample, ".pdf", sep = ""))
   print(plt)
   dev.off()
   #plt <- plt + stat_density(aes(x = components), position = "identity", geom="line")
