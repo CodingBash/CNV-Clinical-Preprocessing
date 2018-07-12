0a298e8d9b13a85bd23a9a5af6058014ac6c330c
CodingBash <bbecerr@outlook.com>
2018-07-12 15:00:38 -0400

    Visualize GMM notebook, now added way to visualize more than 1 sample

--- notebooks/Visualize GMMs from CNprep.r
+++ notebooks/Visualize GMMs from CNprep.r
@@ -3,14 +3,18 @@
 source("helperFunctions.R")
 source("visualizeGMMLibrary.R")
 
-sample <- "hT3" # Set the sample that you want to view the GMM for here.
+sample_list <- c("hT3", "hT30") # Set the sample that you want to view the GMM for here.
 
-cd_local()
-segtable_mclust_V <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run3/")
-segtable_mclust_E <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run1/")
+for(sample in sample_list){
+    cd_local()
+    segtable_mclust_V <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run3/")
+    displayGMM(segtable = segtable_mclust_V, sample = sample, print = TRUE, save = FALSE)
+}
 
-displayGMM(segtable = segtable_mclust_V, sample = sample, print = TRUE, save = FALSE)
-
-displayGMM(segtable = segtable_mclust_E, sample = sample, print = TRUE, save = FALSE)
+for(sample in sample_list){
+    cd_local()
+    segtable_mclust_E <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run1/")
+    displayGMM(segtable = segtable_mclust_E, sample = sample, print = TRUE, save = FALSE)
+}
 
 
