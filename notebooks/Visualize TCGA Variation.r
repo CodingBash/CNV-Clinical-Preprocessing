05ce4e6e54006297e5a8caee649bef27ea833b34
CodingBash <bbecerr@outlook.com>
2018-07-19 17:59:15 -0400

    Produced TCGA visualization output in notebook

--- notebooks/Visualize TCGA Variation.r
+++ notebooks/Visualize TCGA Variation.r
@@ -9,20 +9,21 @@ source("visualizeTCGAFacetsVariationLibrary.R")
 
 
 
+
 visualizeSegmentAndSnpSDAllTumor <- function() {
   setwd(paste0("~/Documents/Git-Projects/Git-Research-Projects/FACETS_nonmatching_test/"))
-  for(tumorId in seq(2, 2)){
+  for(tumorId in seq(2, 7)){
     seg_sd_bed <- calculateSegmentSD(tumorId)
     snp_sd_bed <- calculateSnpSD(tumorId)
     visualizeCNProfile(title = paste0("Segment SNP SD Profile for tumorId=", tumorId), facets_snp_data = snp_sd_bed, line_data = seg_sd_bed, save = FALSE, ymin = 0, ymax = 1)
   }
 }
-#facets_snp_data = snp_sd_bed, 
+
 visualizeSegmentAndSnpSDAllTumor()
 
 visualizeSegmentAndSnpRMSEAllTumor <- function() {
   setwd(paste0("~/Documents/Git-Projects/Git-Research-Projects/FACETS_nonmatching_test/"))
-  for(tumorId in seq(1, 7)){
+  for(tumorId in seq(2, 7)){
     seg_rmse_bed <- calculateSegmentRMSE(tumorId)
     snp_rmse_bed <- calculateSnpRMSE(tumorId)
     visualizeCNProfile(paste0("Segment SNP RMSE Profile for tumorId=", tumorId), facets_snp_data = snp_rmse_bed, line_data = seg_rmse_bed, categories = c("chr14"), save = FALSE, ymin = 0, ymax = 1)
