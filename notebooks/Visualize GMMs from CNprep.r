f043af78beddc052fdc9a670d5c97148928d6aa4
CodingBash <bbecerr@outlook.com>
2018-07-19 16:12:51 -0400

    Visualized CNprep results for model E and minjoin = 0.50 to hopefully view more clusters

--- notebooks/Visualize GMMs from CNprep.r
+++ notebooks/Visualize GMMs from CNprep.r
@@ -3,13 +3,7 @@ setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
 source("helperFunctions.R")
 source("visualizeGMMLibrary.R")
 
-sample_list <- c("hT30", "hT65","hT70", "hT72") # Set the sample that you want to view the GMM for here.
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable_mclust_V <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run3/")
-    displayGMM(segtable = segtable_mclust_V, sample = sample, print = TRUE, save = FALSE)
-}
+sample_list <- c("hT1", "hT25","hT70", "hT72") # Set the sample that you want to view the GMM for here.
 
 for(sample in sample_list){
     setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
@@ -19,56 +13,12 @@ for(sample in sample_list){
 
 for(sample in sample_list){
     setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable_mclust_V_minJoin_05 <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_2/")
-    displayGMM(segtable = segtable_mclust_E, sample = sample, print = TRUE, save = FALSE)
+    segtable_mclust_V <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run3/")
+    displayGMM(segtable = segtable_mclust_V, sample = sample, print = TRUE, save = FALSE)
 }
 
 for(sample in sample_list){
     setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable_mclust_E_minJoin_05 <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_3/")
-    displayGMM(segtable = segtable_mclust_E, sample = sample, print = TRUE, save = FALSE)
+    segtable_mclust_V <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_19_2018_1/")
+    displayGMM(segtable = segtable_mclust_V, sample = sample, print = TRUE, save = FALSE)
 }
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_14/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_15/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_16/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_17/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_18/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_19/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-for(sample in sample_list){
-    setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
-    segtable <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_15_2018_20/")
-    displayGMM(segtable = segtable, sample = sample, print = TRUE, save = FALSE)
-}
-
-
