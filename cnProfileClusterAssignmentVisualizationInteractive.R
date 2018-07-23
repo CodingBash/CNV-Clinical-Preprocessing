01530672e5e8aebcee50187d8e0de8b91ad2b140
CodingBash <bbecerr@outlook.com>
2018-07-23 12:25:57 -0400

    Organization visualization script - made notebooks

--- cnProfileClusterAssignmentVisualizationInteractive.R
+++ cnProfileClusterAssignmentVisualizationInteractive.R
@@ -8,66 +8,18 @@
 
 #
 # Load sources
-# WARNING: If sources don't load, manually setwd() to src file location
 #
 setwd("~/Git-Projects/Git-Research-Projects/drug-response-prediction")
-source("cnProfileVisualizationLibrary.R")
-source("helperFunctions.R")
-source("visualizeGMMLibrary.R")
+source("cnProfileClusterAssignmentVisualizationLibrary.R")
 
+#
+# Prepare parameters
+#
 cd_local()
-# Load samples to generate CN profiles for
 loaded_samples <- load_samples(classes = c("T"), sampleList = "sampleList.csv")
-
 chromosomeSizes <- readRDS("./chromosomeSizes.rds")
+base_axis <- generateBaseAxis(chromosomeSizes)
 
-base_axis <- data.frame()
-for(chromosomeSizes.index in seq_along(chromosomeSizes$size)){
-  entry <- data.frame(chrom = chromosomeSizes[chromosomeSizes.index, ]$chrom, start = 1, end = chromosomeSizes[chromosomeSizes.index, ]$size, value = 0.0)
-  base_axis <- rbind(base_axis, entry)
-}
-
-
-
-#head(loaded_samples,5)
-#
-# Iterate through each sample and generate CN profile
-#
 for(sample in loaded_samples){
-  #
-  # Retrieve and format input data
-  #
-  cd_local()
-  facets_segment_clusters <- retrieveSegtable(sample, dir = "segClusteringResults/prev_run_7_19_2018_2/")
-  gaussian_comps <- unique(facets_segment_clusters[,c("maxzmean", "maxzsigma")])
-  row.names(gaussian_comps) <- seq_along(row.names(gaussian_comps))
-  component_indices <- unlist(lapply(seq(1, nrow(facets_segment_clusters)), function(index){
-    # Return the index of the GMM component that the segment belongs to
-    return(as.numeric(rownames(gaussian_comps[gaussian_comps$maxzmean == facets_segment_clusters[index, ]$maxzmean & gaussian_comps$maxzsigma == facets_segment_clusters[index, ]$maxzsigma, ])))
-  }))
-  # Set the segment index as the corrensponding name of each component index
-  
-  
-  facets_segment_clusters_bed <- segmentClustersToBedFormat(facets_segment_clusters, value = 5)
-  indices <- unlist(lapply(seq(1, nrow(facets_segment_clusters)), function(index){
-    if(facets_segment_clusters[index, ]$chrom.pos.start %in% facets_segment_clusters_bed$start & facets_segment_clusters[index, ]$chrom.pos.end %in% facets_segment_clusters_bed$end){
-      return(index)
-    }
-  }))
-  
-  
-  component_indices <- component_indices[indices]
-  
-  names(component_indices) <- rownames(facets_segment_clusters_bed)
-  prev_length <- nrow(facets_segment_clusters_bed)
-  facets_segment_clusters_bed <- rbind(facets_segment_clusters_bed, base_axis)
-  axis_indices <- rep(6, nrow(base_axis))
-  names(axis_indices) <- seq(prev_length+ 1, nrow(facets_segment_clusters_bed))
-  component_indices <- append(component_indices, axis_indices)
-  
-  #
-  # Generate CN profile from input data
-  #
-  cd_local()
-  visualizeCNProfile(facets_segment_data = facets_segment_clusters_bed, color_id = component_indices, save = TRUE, saveDir = "./clusterAssignmentVisualization/", saveMeta = paste0("clusterassignment_", sample), title=paste0("CNprep Cluster Assignment for sample=", sample))
+  visualizeClusterAssignmnet(sample, save = FALSE, saveDir = "./clusterAssignmentVisualization/")
 }
---
+++ notebooks/Visualize CNPrep Cluster Assignment.r
@@ -0,0 +1,34 @@
+
+setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
+source("cnProfileClusterAssignmentVisualizationLibrary.R")
+
+chromosomeSizes <- readRDS("./chromosomeSizes.rds")
+base_axis <- generateBaseAxis(chromosomeSizes)
+
+# Set the directory of the CNprep segtable results
+segDir <- "segClusteringResults/prev_run1/"
+
+# Set the sample that you want to view the CN profile for here.
+loaded_samples <- c("hT1", "hT25","hT70", "hT72")
+
+# With the 0-axis
+for(sample in loaded_samples){
+  visualizeClusterAssignment(sample, base_axis = base_axis, segDir = segDir, save = FALSE, saveDir = "./clusterAssignmentVisualization/")
+}
+
+# Without the 0-axis
+for(sample in loaded_samples){
+  visualizeClusterAssignment(sample, segDir = segDir, save = FALSE, saveDir = "./clusterAssignmentVisualization/")
+}
+
+loaded_samples <- load_samples(classes = c("T"), sampleList = "sampleList.csv")
+
+for(sample in loaded_samples){
+  visualizeClusterAssignment(sample, base_axis = base_axis, segDir = segDir, save = FALSE, saveDir = "./clusterAssignmentVisualization/")
+}
+
+for(sample in loaded_samples){
+  visualizeClusterAssignment(sample, segDir = segDir, save = FALSE, saveDir = "./clusterAssignmentVisualization/")
+}
+
+
