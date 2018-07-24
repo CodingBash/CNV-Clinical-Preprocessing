43d4cb475f88f65f18a754ef40d1cbe9d0531e64
CodingBash <bbecerr@outlook.com>
2018-07-24 19:42:51 -0400

    Added documentation to the CNLR CORE Feature notebook

--- notebooks/Visualize CNLR CORE Feature Assignment.r
+++ notebooks/Visualize CNLR CORE Feature Assignment.r
@@ -3,15 +3,8 @@ setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction")
 source("genomicFeatureAssignment.R")
 
 
-
-#
-# Load sample to retrieve feature set for
-#
 samples <- load_samples(classes = c("T", "M", "F"))
 
-#
-# Retrieve CORE features
-#
 setwd("~/Documents/Git-Projects/Git-Research-Projects/drug-response-prediction/hT_results/finalCores/prev_run_7_3_2018")
 Acores <- retrieveCores("AfinalCoresBP_subtractFraction.bed") # BED file of amplification recurrent regions
 Dcores <- retrieveCores("DfinalCoresBP_subtractFraction.bed") # BED file of deletion recurrent regions
@@ -23,7 +16,7 @@ options(warn=-1)
 # Retrieve training set
 #
 setwd("~/Documents")
-training_set <- retrieveTrainingSet(, Acores, Dcores, binDir = "CSHL/Project_TUV_12995_B01_SOM_Targeted.2018-03-02/")
+training_set <- retrieveTrainingSet(samples, Acores, Dcores, binDir = "CSHL/Project_TUV_12995_B01_SOM_Targeted.2018-03-02/")
 
 visualizeUnclusteredHeatmap(training_set)
 
