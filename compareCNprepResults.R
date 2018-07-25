c7d023e984d1835736e1e9ec205fb9cb5b0255b4
CodingBash <bbecerr@outlook.com>
2018-07-25 16:03:07 -0400

    Near completed draft of CNprep comparison visualization

--- compareCNprepResults.R
+++ compareCNprepResults.R
@@ -2,45 +2,123 @@ setwd("~/Git-Projects/Git-Research-Projects/drug-response-prediction")
 source("helperFunctions.R")
 library(RColorBrewer)
 
+#
+# Create dataframe of available CNprep runs
+#
 all_model_specs <- data.frame(stringsAsFactors = FALSE)
-cnprep_run_1 <- data.frame(dir="prev_run1", model="E", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
-cnprep_run_2 <- data.frame(dir="prev_run3", model="V", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
-cnprep_run_3 <- data.frame(dir="prev_run_7_19_2018_1", model="E", minjoin=0.50, ntrial = 40, stringsAsFactors = FALSE)
-cnprep_run_4 <- data.frame(dir="prev_run_7_19_2018_2", model="E", minjoin=1.00, ntrial = 40, stringsAsFactors = FALSE)
-cnprep_run_5 <- data.frame(dir="prev_run_7_19_2018_3", model="V", minjoin=1.00, ntrial = 10, stringsAsFactors = FALSE)
-all_model_specs <- rbind(cnprep_run_1, cnprep_run_2, cnprep_run_3, cnprep_run_4, cnprep_run_5)
+cnprep_run <- list()
+cnprep_run[[1]] <- data.frame(dir="prev_run1", model="E", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[2]] <- data.frame(dir="prev_run3", model="V", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[3]] <- data.frame(dir="prev_run_7_15_2018_14", model="E", minjoin=0.25, ntrial = 40, stringsAsFactors = FALSE)
+cnprep_run[[4]] <- data.frame(dir="prev_run_7_15_2018_15", model="E", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[5]] <- data.frame(dir="prev_run_7_15_2018_16", model="V", minjoin=0.25, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[6]] <- data.frame(dir="prev_run_7_15_2018_17", model="E", minjoin=0.01, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[7]] <- data.frame(dir="prev_run_7_15_2018_18", model="V", minjoin=0.01, ntrial = 10, stringsAsFactors = FALSE)
+cnprep_run[[8]] <- data.frame(dir="prev_run_7_15_2018_19", model="E", minjoin=0.01, ntrial = 50, stringsAsFactors = FALSE)
+cnprep_run[[9]] <- data.frame(dir="prev_run_7_15_2018_18", model="V", minjoin=0.01, ntrial = 50, stringsAsFactors = FALSE)
+cnprep_run[[10]] <- data.frame(dir="prev_run_7_15_2018_18", model="V", minjoin=0.01, ntrial = 50, stringsAsFactors = FALSE)
+cnprep_run[[11]] <- data.frame(dir="prev_run_7_19_2018_1", model="E", minjoin=0.50, ntrial = 40, stringsAsFactors = FALSE)
+cnprep_run[[12]] <- data.frame(dir="prev_run_7_19_2018_2", model="E", minjoin=1.00, ntrial = 40, stringsAsFactors = FALSE)
+cnprep_run[[13]] <- data.frame(dir="prev_run_7_19_2018_3", model="V", minjoin=1.00, ntrial = 10, stringsAsFactors = FALSE)
+all_model_specs <- do.call(rbind, cnprep_run)
 
-displayCNResults <- function(organoidId, bin_start, bin_end, model_specs, values, cols, hl){
+#
+# Method to display and compare CNprep results
+#
+# @param("organoidId") - string of the organoid sample
+# @param("bin_start") - start of the x-axis (in bin units)
+# @param("bin_end") - end of the x-axis (in bin units)
+# @param("model_specs") - list of CNprep runs to compare
+# @param("cluster_value") - value of the segtable column with the cluster values
+# @param("supplementary_values") - values of the segtable columns with any supplementary data to overlay
+# @param("cluster_cols") - color palette for each cluster
+# @param("supplementary_cols") - color palette for each supplementary data
+# @param("hl") - boolean declaring if plot horizontal lines should appear
+#
+displayCNprepResults <- function(organoidId, bin_start, bin_end, model_specs, cluster_value = "maxzmean", supplementary_values, cluster_cols, supplementary_cols, hl = TRUE){
+
+  #
+  # Set plot parameters
+  #
   par(mfrow=c(nrow(model_specs),1)) 
   par(mar=c(2,2,1.25,1))
-  #par(cex.main = .5, cex.axis = 0.5)
   
   #
-  # Calculate plot range
+  # Retrieve list of all CNprep segtables (accounting for bin range)
   #
-  all_segtables <- lapply(seq(1, nrow(model_specs)), function(model_specs.index){
+  all_segtables <- lapply(seq(1, nrow(model_specs)), function(model_specs.index, bin_start, bin_end){
     segtable <- retrieveSegtable(organoidId, dir = paste0("segClusteringResults/", model_specs[model_specs.index, ]$dir, "/"))
-    segtable <- segtable[segtable$start >= bin_start & segtable$end <= bin_end,]
+    if(!missing(bin_start) & !missing(bin_end)){
+      segtable <- segtable[segtable$start >= bin_start & segtable$end <= bin_end,]  
+    } else if (!missing(bin_start)){
+      segtable <- segtable[segtable$start >= bin_start,]  
+    } else if (!missing(bin_end)) {
+      segtable <- segtable[segtable$end <= bin_end,]  
+    }
     return(segtable)
   })
   
+  #
+  # Generate dataframe with all CNprep segtables
+  #
   binded_segtables <- do.call(rbind, all_segtables)
-  
+
+  #
+  # Determine plot ranges
+  #
+  values <- list()
+  if(!missing(cluster_value)){
+    values <- c(values, cluster_value)
+  }
+  if(!missing(supplementary_values)){
+    values <- c(values, supplementary_values)
+  }
   ymargin <- 0.1
   yvalues <- sapply(values, function(value){binded_segtables[[value]]})
   xrange <- range(binded_segtables$start,binded_segtables$end)
   yrange <- range(yvalues - ymargin, yvalues + ymargin)
     
+  #
+  # Iterate through each CNprep segtable
+  #
   for(segtable.i in seq_along(all_segtables)){
+    #
+    # Store necessary data information
+    #
     segtable <- all_segtables[[segtable.i]]
     model_spec <- model_specs[segtable.i, ]
+    
+    #
+    # Set plot information
+    #
     as.list(model_spec)
     title <- paste0("dir=", model_spec$dir, " model=", model_spec$model, " minjoin=", model_spec$minjoin, " ntrial=", model_spec$ntrial)
     plot(xrange, yrange, main = title, type="n", xlab = "", ylab = "")
-    for(value.i in seq_along(values)){
-      segments(x0 = segtable$start, x1 = segtable$end, y0 = segtable[[values[[value.i]]]], y1 = segtable[[values[[value.i]]]],
-               col = cols[[value.i]], lty = par("lty"), lwd = par("lwd"))
+    
+    #
+    # Display supplementary value segments
+    #
+    if(!missing(supplementary_values)){
+      for(supplementary_value.index in seq_along(supplementary_values)){
+        segments(x0 = segtable$start, x1 = segtable$end, y0 = segtable[[supplementary_values[[supplementary_value.index]]]], y1 = segtable[[supplementary_values[[supplementary_value.index]]]],
+                 col = supplementary_cols[[supplementary_value.index]], lty = par("lty"), lwd = par("lwd"))
+      }
     }
+    
+    #
+    # Display clusters
+    #
+    if(!missing(cluster_value)){
+        clusters <- split(segtable, f=segtable[[cluster_value]])
+        for(cluster.index in seq_along(clusters)){
+          segments(x0 = clusters[[cluster.index]]$start, x1 = clusters[[cluster.index]]$end, y0 = clusters[[cluster.index]][[cluster_value]], y1 = clusters[[cluster.index]][[cluster_value]],
+                   col = cluster_cols[[cluster.index]], lty = par("lty"), lwd = par("lwd"))
+        }
+    }
+    
+    #
+    # Display horizontal lines
+    #
     if(hl == TRUE){
       abline(h=0)
       abline(h=0.5, col = "#4F4F4F")
@@ -51,6 +129,6 @@ displayCNResults <- function(organoidId, bin_start, bin_end, model_specs, values
   }
 }
 
-cluster_cols <- brewer.pal(n = 7, name="Set1")
-supplementary_cols <- brewer.pal(n = 7, name="Set3")
-displayCNResults(organoidId= "hT1", bin_start = 20000, bin_end = 40000, model_specs = rbind(cnprep_run_4,cnprep_run_2 ), cluster_value = "maxzmean", supplementary_values = c("segmedian", "mediandev"), cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, hl = TRUE)
+cluster_cols <- brewer.pal(n = 7, name="Dark2")
+supplementary_cols <- brewer.pal(n = 7, name="Set2")
+displayCNprepResults(organoidId= "hT1", model_specs = all_model_specs[c(1,3,4), ], cluster_value = "maxzmean", cluster_cols = cluster_cols, supplementary_cols =  supplementary_cols, hl = TRUE)
