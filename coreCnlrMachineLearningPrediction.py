84087dd0f2e462d6b4732cfcd646d08edd2ddbbe
CodingBash <bbecerr@outlook.com>
2018-07-26 17:43:07 -0400

    Output of rough draft of ml model - several changes to make

--- coreCnlrMachineLearningPrediction.py
+++ coreCnlrMachineLearningPrediction.py
@@ -19,16 +19,47 @@ plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
             
 import pandas as pd
+import scipy
 
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+import math
+
+# TODO: Manipulate test_ratio
+def split_train_test(training_set, test_ratio = 0.33):
+    row_count = training_set.shape[0]
+    shuffled_indices = np.random.permutation(row_count)
+    test_set_size = int(test_ratio * row_count)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return training_set.iloc[train_indices], training_set.iloc[test_indices]
+    
+    
 labeled_matrix_training_set = pd.read_csv("mlOutput/coreTrainingSet_7_26_2018_1.csv")
 labeled_matrix_training_set.columns.values[0] = "sampleId"
 
-labels = 1:6
+
+labels = list(range(1,6))
+
 for label in labels:
     # Remove uneeded labels
     selected_training_set = labeled_matrix_training_set.iloc[:, list([0]) + list([label]) + list(range(6,labeled_matrix_training_set.shape[1]))].copy()
-    
-    
-
-
-split_train_test()
+    selected_training_set = selected_training_set[np.isfinite(selected_training_set.iloc[:,1])]
+    training_set, testing_set = split_train_test(selected_training_set)
+    model_data = training_set.copy().drop([training_set.columns[0], training_set.columns[1]], axis = 1)
+    model_labels = training_set.iloc[:,1]
+    lasso = LinearRegression()
+    lasso.fit(model_data, model_labels)
+    model_test_data = testing_set.copy().drop([training_set.columns[0], training_set.columns[1]], axis = 1)
+    model_test_labels = testing_set.iloc[:,1]
+    predictions = lasso.predict(model_test_data)
+    mse = mean_squared_error(model_test_labels, predictions)
+    rmse = np.sqrt(mse)
+    print(rmse)
+    r = scipy.stats.pearsonr(model_test_labels, predictions)
+    t = scipy.stats.spearmanr(model_test_labels, predictions)
+    print("Pearson: " + str(r))
+    print("Spearman: " + str(t))
+    plt.plot(model_test_labels, predictions, 'bo')
+    plt.show()
