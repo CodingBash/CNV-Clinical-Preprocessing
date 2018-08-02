16ded74e4a26608109f682b5a662576af219cd77
CodingBash <bbecerr@outlook.com>
2018-08-02 11:51:47 -0400

    Updated and refactored ML code

--- notebooks/Supervised Learning using CNLR COREs.py
+++ notebooks/Supervised Learning using CNLR COREs.py
@@ -52,7 +52,7 @@ import math
 
 # ### Define method to split training and testing set
 
-# In[34]:
+# In[174]:
 
 # TODO: Manipulate test_ratio
 def split_train_test(training_set, test_ratio = 0.33):
@@ -66,22 +66,58 @@ def split_train_test(training_set, test_ratio = 0.33):
 
 # ### Load training set matrix
 
-# In[100]:
+# In[177]:
 
-labeled_matrix_training_set = pd.read_csv("../mlOutput/coreTrainingSet_7_26_2018_1.csv")
+labeled_matrix_training_set = pd.read_csv("../mlOutput/coreTrainingSet_7_31_2018_1.csv")
 #labeled_matrix_training_set.columns.values[0] = "sampleId"
 labeled_matrix_training_set = labeled_matrix_training_set.drop([labeled_matrix_training_set.columns[0]], axis = 1)
 labels = list(range(0,5))
 
 
-# In[101]:
+# In[178]:
 
-display(labeled_matrix_training_set.copy().head())
+display(labeled_matrix_training_set.head(25))
+
+
+# In[179]:
+
+X = labeled_matrix_training_set.copy().drop(labeled_matrix_training_set.columns[labels], axis = 1)
+y = labeled_matrix_training_set.copy()[labeled_matrix_training_set.columns[labels]]
+
+
+# In[180]:
+
+display(X.head())
+
+
+# In[198]:
+
+display(y.head(15))
+
+
+# In[200]:
+
+from sklearn.model_selection import train_test_split
+
+all_X_TRAIN, all_X_TEST, all_Y_TRAIN, all_Y_TEST = train_test_split(X, y, test_size=0.20, random_state=42)
+# TODO: train_test must be split on amount of NAs as well!
+
+
+# In[201]:
+
+display(all_X_TRAIN.head())
+display(all_Y_TRAIN.head())
+
+
+# In[202]:
+
+display(all_X_TEST.head())
+display(all_Y_TEST.head())
 
 
 # ## Visualize ML Results
 
-# In[38]:
+# In[169]:
 
 def abline(slope, intercept):
     """Plot a line from slope and intercept"""
@@ -91,25 +127,7 @@ def abline(slope, intercept):
     plt.plot(x_vals, y_vals, '--')
 
 
-# In[109]:
-
-#selected_training_set.head()
-#y = selected_training_set.copy()[[selected_training_set.columns[0]]]
-#X = selected_training_set.copy().drop([selected_training_set.columns[0]], axis=1)
-
-X = labeled_matrix_training_set.copy().drop(labeled_matrix_training_set.columns[labels], axis = 1)
-y = labeled_matrix_training_set.copy()[labeled_matrix_training_set.columns[labels]]
-
-
-# In[111]:
-
-from sklearn.model_selection import train_test_split
-
-X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X, y, test_size=0.33, random_state=42)
-# TODO: Using the above objects instead
-
-
-# In[124]:
+# In[230]:
 
 def retrieve_pipelines(model_name, ml_model):
     Ypipeline = Pipeline([
@@ -126,147 +144,118 @@ def retrieve_pipelines(model_name, ml_model):
     return (Ypipeline, XYpipeline)
 
 def imputer_inverse_transform(pre_data, post_data):
-        na_indices = np.where(np.isnan(pre_data))[0]
-        pprint(na_indices)
-        post_data[na_indices] = float('NaN')
-        return post_data
-
-
-# ### Visualize ML results using Linear Regression
-
-# In[125]:
-
-for label in labels:
-    Ypipeline, XYpipeline = retrieve_pipelines("ridge_model", Ridge(alpha = 0.8))
-    this_y_train = Y_TRAIN.iloc[:,label]
-    this_y_test = Y_TEST.iloc[:,label]
+    na_indices = np.where(np.isnan(pre_data))[0]
+    pprint(na_indices)
+    post_data[na_indices] = float('NaN')
+    return post_data
     
-    # TODO: Y contains all labels - need to subselect one based on label variable
-    y_train_tr = Ypipeline.fit_transform(this_y_train)
-    XYpipeline.fit(X_TRAIN,this_y_train)
+def remove_NAs(X, y, label):
+    label_y = y[[y.columns[label]]]
+    na_indices = label_y[label_y.columns[0]].index[label_y[label_y.columns[0]].apply(np.isnan)]
+    y_nonNA = label_y.copy().drop(na_indices)
+    X_nonNA = X.copy().drop(na_indices)
+    return X_nonNA, y_nonNA
+    
+def train_and_test(Ypipeline, XYpipeline, X_TRAIN, X_TEST, this_y_train, this_y_test):
+    this_y_train_tr = Ypipeline.fit_transform(this_y_train)
+    XYpipeline.fit(X_TRAIN,this_y_train_tr)
 
-    y_test_tr = Ypipeline.transform(Y_TEST.iloc[:,label])
+    y_test_tr = Ypipeline.transform(this_y_test)
     y_prediction = XYpipeline.predict(X_TEST)
 
     y_prediction = Ypipeline.named_steps['standardizer'].inverse_transform(y_prediction)
-    y_prediction = imputer_inverse_transform(Y_TEST.iloc[:,label], y_prediction)
+    y_prediction = imputer_inverse_transform(this_y_test, y_prediction)
 
-    y_test_np = Y_TEST.iloc[:,label].copy().values.flatten()
+    y_test_np = this_y_test
     y_test_np = y_test_np[~np.isnan(y_test_np)]
     y_prediction = y_prediction[~np.isnan(y_prediction)]
+    return (y_test_np, y_prediction, this_y_train_tr)
 
+
+def simple_score(y_test_np, y_prediction):
     rmse = np.sqrt(mean_squared_error(y_test_np, y_prediction))
     r = scipy.stats.pearsonr(y_test_np, y_prediction)
     t = scipy.stats.spearmanr(y_test_np, y_prediction)
-
-    print("RMSE: " + str(rmse))
-    print("Pearson: " + str(r))
-    print("Spearman: " + str(t))
-
+    return (rmse, r, t)
+    
+def visualize(y_test_np, y_prediction):
     plt.plot(y_test_np, y_prediction, 'bo')
     abline(1,0)
     plt.ylabel("Prediction")
     plt.xlabel("Label")
     plt.show()
+    
+def display_set(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST):
+    TRAIN = pd.concat([Y_TRAIN, X_TRAIN], axis=1)
+    TEST = pd.concat([Y_TEST, X_TEST], axis=1)
+    pprint("TRAIN")
+    display(TRAIN)
+    pprint("TEST")
+    display(TEST)
 
-    scores = cross_val_score(XYpipeline, x_train, y_train_tr,
+def cv_score(XYpipeline, X_TRAIN, this_y_train_tr):
+    scores = cross_val_score(XYpipeline, X_TRAIN, this_y_train_tr,
                              scoring = "neg_mean_squared_error", cv=10)
-    
     scores = Ypipeline.named_steps['standardizer'].inverse_transform(scores)
-    
-    print("CV Scores: " + str(scores))
-    print("CV Mean: " + str(scores.mean()))
-    print("CV STD: " + str(scores.std()))
+    return scores
 
 
-# ### Visualize ML results using Random Forest Regressor
+# ### Visualize ML results using Linear Regression
 
-# In[41]:
+# In[231]:
 
 for label in labels:
-    #
-    # Select label column
-    #
-    selected_training_set = labeled_matrix_training_set.iloc[:, list([label]) + list(range(6,labeled_matrix_training_set.shape[1]))].copy()
-    selected_training_set = selected_training_set[~np.isnan(selected_training_set.iloc[:,0])]
-    #
-    # Divide into training set and testing set
-    #
-    training_set, testing_set = split_train_test(selected_training_set, test_ratio = 0.33) # TODO: Use sklearn's train_test_split
+    X_nonNA, y_nonNA = remove_NAs(X, y, label)
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X_nonNA, y_nonNA, test_size=0.20)
+    
+    display_set(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST)
+        
+    Ypipeline, XYpipeline = retrieve_pipelines("ridge_model", Ridge(alpha = 0.8))
+    
+    y_test_np, y_prediction, this_y_train_tr = train_and_test(Ypipeline, XYpipeline, X_TRAIN, X_TEST, Y_TRAIN.values, Y_TEST.values)
 
-    #
-    # Get model training information and preprocess
-    #
-    model_data = training_set.copy().drop([training_set.columns[0]], axis = 1)
-    model_labels = training_set.copy()[[training_set.columns[0]]]
-
-    Ypipeline = Pipeline([
-     ('imputer', Imputer(axis=0,strategy="median")),
-     ('standardizer', StandardScaler()),
-    ])
-
-    model_labels_tr = Ypipeline.fit_transform(model_labels)
-
-    XYpipeline = Pipeline([
-            ('pca', decomposition.PCA(n_components=15)),
-            ('imputer', Imputer(axis=0,strategy="median")),
-            ('standardizer', StandardScaler()),
-            ('rf_model', RandomForestRegressor(n_estimators=100, max_leaf_nodes=16, n_jobs=4)) # TODO: For now, hardcore the parameters
-    ])
-
-    XYpipeline.fit(model_data, model_labels_tr)
-
-    #
-    # TODO: To prevent data leakage, separate the scope after the model has been fit
-    #
-
-    #
-    # Get model testing information and preprocess
-    #
-    model_test_data = testing_set.copy().drop([testing_set.columns[0]], axis = 1)
-    model_test_labels = testing_set.copy()[[testing_set.columns[0]]]
-
-    model_test_labels_tr = Ypipeline.transform(model_test_labels)
-    predictions = XYpipeline.predict(model_test_data)
-
-    def imputer_inverse_transform(pre_data, post_data):
-        na_indices = np.where(np.isnan(pre_data))[0]
-        pprint(na_indices)
-        post_data[na_indices] = float('NaN')
-        return post_data
-
-
-    predictions = Ypipeline.named_steps['standardizer'].inverse_transform(predictions)
-    predictions = imputer_inverse_transform(model_test_labels, predictions)
-
-    model_test_labels = model_test_labels.copy().values.flatten()
-    model_test_labels = model_test_labels[~np.isnan(model_test_labels)]
-    predictions = predictions[~np.isnan(predictions)]
-
-    rmse = np.sqrt(mean_squared_error(model_test_labels, predictions))
-    r = scipy.stats.pearsonr(model_test_labels, predictions)
-    t = scipy.stats.spearmanr(model_test_labels, predictions)
-
+    rmse, r, t = simple_score(y_test_np, y_prediction)
+    
     print("RMSE: " + str(rmse))
     print("Pearson: " + str(r))
     print("Spearman: " + str(t))
 
-    plt.plot(model_test_labels, predictions, 'bo')
-    abline(1,0)
-    plt.ylabel("Prediction")
-    plt.xlabel("Label")
-    plt.show()
-
-    #scores = cross_val_score(XYpipeline, model_data, model_labels_tr,
-                             #scoring = "neg_mean_squared_error", cv=10)
-    #scores = Ypipeline.named_steps['standardizer'].inverse_transform(scores)
+    #scores = cv_score(XYpipeline, X_TRAIN, this_y_train_tr)
     
     #print("CV Scores: " + str(scores))
     #print("CV Mean: " + str(scores.mean()))
     #print("CV STD: " + str(scores.std()))
+             
+    visualize(y_test_np, y_prediction)
 
 
+# ### Visualize ML results using Random Forest Regressor
 
+# In[232]:
+
+for label in labels:
+    X_nonNA, y_nonNA = remove_NAs(X, y, label)
+    X_TRAIN, X_TEST, Y_TRAIN, Y_TEST = train_test_split(X_nonNA, y_nonNA, test_size=0.20)
+    
+    display_set(X_TRAIN, X_TEST, Y_TRAIN, Y_TEST)
+        
+    Ypipeline, XYpipeline = retrieve_pipelines("rfs_model", RandomForestRegressor(n_estimators=1000, max_leaf_nodes=8, n_jobs=4))
+    
+    y_test_np, y_prediction, this_y_train_tr = train_and_test(Ypipeline, XYpipeline, X_TRAIN, X_TEST, Y_TRAIN.values, Y_TEST.values)
+
+    rmse, r, t = simple_score(y_test_np, y_prediction)
+    
+    print("RMSE: " + str(rmse))
+    print("Pearson: " + str(r))
+    print("Spearman: " + str(t))
+
+    #scores = cv_score(XYpipeline, X_TRAIN, this_y_train_tr)
+    
+    #print("CV Scores: " + str(scores))
+    #print("CV Mean: " + str(scores.mean()))
+    #print("CV STD: " + str(scores.std()))
+             
+    visualize(y_test_np, y_prediction)
 
 
 # In[ ]:
